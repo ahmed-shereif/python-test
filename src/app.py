6f2b9c2b2f4c937f353a66737ec88ced72d0ec7e
ahmed-shereif <ahmed1996sherif@gmail.com>
2026-07-11 02:15:45 +0300

    m2

--- src/app.py
+++ src/app.py
@@ -11,7 +11,8 @@ def get_details():
         'version': '1.0',
         'description': 'This is a sample API endpoint.',
         'hostname': socket.gethostname(),
-        'message': 'Hello from the API!'
+        'message': 'Hello from the API!',
+        "m2": "This is a sample message from the API."
     }
     return jsonify(details)
 
