9705286bdcf56e3810bf1f9f195962b2c9ff1c49
ahmed-shereif <ahmed1996sherif@gmail.com>
2026-07-13 15:16:15 +0300

    m5

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ def get_details():
         "m2": "This is a sample message from the API.",
         'm3': 'This is another sample message from the API.',
         'm4': 'This is yet another sample message from the API.',
+        'm5': 'This is a fifth sample message from the API.',
     }
     return jsonify(details)
 
